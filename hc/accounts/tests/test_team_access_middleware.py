9f7e8907756b4d6a4eab603365c2e2d61f4aabfd
matthewacha <matthewacha@gmail.com>
2018-06-22 16:18:47 +0300

    [feature] fix accounts test
    add asserts in the test cases
    add doc strings
    breakdown the tests so that each test case tests single element.
    add other tests
    
    [Delivers #158174587]

--- hc/accounts/tests/test_team_access_middleware.py
+++ hc/accounts/tests/test_team_access_middleware.py
@@ -6,12 +6,16 @@ from hc.accounts.models import Profile
 class TeamAccessMiddlewareTestCase(TestCase):
 
     def test_it_handles_missing_profile(self):
+        """test it handles a missing profile"""
         user = User(username="ned", email="ned@example.org")
+        user2 = User(username="nedi", email="nedii@example.org")
         user.set_password("password")
+        user2.set_password("password")
         user.save()
+        user2.save()
 
         self.client.login(username="ned@example.org", password="password")
         r = self.client.get("/about/")
-        self.assertEqual(r.status_code, 200)
-
+        self.client.login(username="nedii@example.org", password="password")
         ### Assert the new Profile objects count
+        assert Profile.objects.count() == 1
