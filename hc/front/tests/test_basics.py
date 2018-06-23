5def67b7f83cd6205dc32ab35355c862d4ca2c09
James Katarikawe <jameskatarikawe@Jamess-MacBook-Pro.local>
2018-06-23 18:21:22 +0300

    Replaced docstrings in test_add_channel.py with comments

--- hc/front/tests/test_basics.py
+++ hc/front/tests/test_basics.py
@@ -6,17 +6,17 @@ from hc.api.models import Check
 class BasicsTestCase(TestCase):
 
     def test_it_shows_welcome(self):
-        r = self.client.get("/")
-        self.assertContains(r, "Get Notified", status_code=200)
+        response = self.client.get("/")
+        self.assertContains(response, "Get Notified", status_code=200)
 
     def test_welcome_code(self):
-        r = self.client.get("/")
+        response = self.client.get("/")
         code = self.client.session["welcome_code"]
         assert Check.objects.filter(code=code).exists()
 
         self.client.session["welcome_code"] = "x"
-        r = self.client.get("/")
+        response = self.client.get("/")
         code = self.client.session["welcome_code"]
-        assert r.status_code == 200
+        assert response.status_code == 200
         assert code != "x"
         assert Check.objects.filter(code=code).exists()
