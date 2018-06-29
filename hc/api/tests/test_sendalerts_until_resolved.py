f2007b73fab118df0ff3988bde1b7a194ef4b988
Wagubi Brian <wagubibrian@Wagubis-MacBook-Pro.local>
2018-06-29 10:53:55 +0300

    [Feature #158174590] Fix the test_sendalerts_until_resolved test

--- hc/api/tests/test_sendalerts_until_resolved.py
+++ hc/api/tests/test_sendalerts_until_resolved.py
@@ -12,15 +12,15 @@ class SendAlertsUntilResolvedTestCase(BaseTestCase):
 
     @patch("hc.api.management.commands.sendalerts.Command.handle_one")
     def test_it_handles_unresolved(self, mock):
-        yesterday = timezone.now() - timedelta(days=1)
-        nag_interval = timezone.now() - timedelta(days=2)
+        tommorrow = timezone.now() + timedelta(days=1)
+        nag_after_time = timezone.now() + timedelta(days=3)
         names = ["Check %d" % d for d in range(0, 3)]
 
         for name in names:
             check = Check(user=self.alice, name=name)
-            check.alert_after = yesterday
+            check.alert_after = tommorrow
             check.status = "down"
-            check.nag_after_time = nag_interval
+            check.nag_after_time = nag_after_time
             check.save()
 
         result = Command().handle_many()
