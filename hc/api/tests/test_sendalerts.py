70e9af8faa10a850ea06f51f46f629e2185aa824
magicmarie <natukunda162@gmail.com>
2018-07-02 13:06:17 +0300

    Fix travis

--- hc/api/tests/test_sendalerts.py
+++ hc/api/tests/test_sendalerts.py
@@ -41,5 +41,4 @@ class SendAlertsTestCase(BaseTestCase):
         Command().handle_one(check)
 
     ### Assert when Command's handle many that when handle_many should return True
-        # assert (Command().handle_many(), True)
-        
+        # assert (Command().handle_many(), True)  
