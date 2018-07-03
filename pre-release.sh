1c0986dcb91e79b163719d26ebab783f43f934d5
matthewacha <matthewacha@gmail.com>
2018-07-03 19:10:38 +0300

    Edit pre-release.py

--- pre-release.sh
+++ pre-release.sh
@@ -1,4 +1,12 @@
-./manage.py makemigrations
-./manage.py migrate
-./manage.py ensuretriggers
-./manage.py sendalerts
+
+echo "Running release tasks"
+
+if [ "$DB" == "postgres" ]; then 
+  echo "Running Migrations"
+  python manage.py makemigrations 
+  python manage.py migrate
+  python manage.py ensuretriggers
+  python manage.py sendalerts
+fi
+
+echo "Done running pre-release.sh"
