4a71822ec2d17c99b4fa64686abfdd4cd7ac53e3
matthewacha <matthewacha@gmail.com>
2018-07-04 10:29:34 +0300

    Edit pre-release.py

--- pre-release.sh
+++ pre-release.sh
@@ -3,6 +3,7 @@ echo "Running release tasks"
 dataBase=$(printenv DB)
 if [ "$dataBase" == "postgres" ]; then 
   echo "Running Migrations"
+  export PATH="/var/run/postgresql/.s.PGSQL.5432:$PATH"
   python manage.py migrate
   python manage.py ensuretriggers
   python manage.py sendalerts
