346396dc49d497fad6c50818122fbfb080bbc124
matthewacha <matthewacha@gmail.com>
2018-07-04 12:41:36 +0300

    Edit pre-release.py

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 release: bash ./pre-release.sh
+addons: add heroku-postgresql
 web: gunicorn hc.wsgi:application
--- pre-release.sh
+++ pre-release.sh
@@ -1,12 +1,11 @@
 
 echo "Running release tasks"
 dataBase=$(printenv DB)
-export PORT=5432
 if [ "$dataBase"=="postgres" ]; then 
   echo "Running Migrations"
   python manage.py migrate
   python manage.py ensuretriggers
   python manage.py sendalerts
 fi
-echo $(printenv PATH)
+echo $dataBase
 echo "Done running pre-release.sh"
