8563878c3bd32900c4787d0ae2c0d7490e5bc600
matthewacha <matthewacha@gmail.com>
2018-07-04 11:58:48 +0300

    Edit pre-release.py

--- pre-release.sh
+++ pre-release.sh
@@ -3,6 +3,10 @@ echo "Running release tasks"
 dataBase=$(printenv DB)
 if [ "$dataBase"=="postgres" ]; then 
   echo "Running Migrations"
+  db:
+  image: postgres:latest
+  ports:
+    - "5432"
   python manage.py migrate
   python manage.py ensuretriggers
   python manage.py sendalerts
