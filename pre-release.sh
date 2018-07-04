28f9824979c98b466dd0c8365221142b8ded4bcc
matthewacha <matthewacha@gmail.com>
2018-07-04 10:04:03 +0300

    Edit pre-release.py

--- pre-release.sh
+++ pre-release.sh
@@ -3,7 +3,7 @@ echo "Running release tasks"
 dataBase=$(printenv DB)
 if [ "$dataBase" == "postgres" ]; then 
   echo "Running Migrations"
-  PATH=/usr/pgsql-9.3/lib:$PATH pip install psycopg2-binary
+  export PATH="/usr/pgsql-9.3/lib:$PATH pip install psycopg2-binary"
   python manage.py migrate
   python manage.py ensuretriggers
   python manage.py sendalerts
