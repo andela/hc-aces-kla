76f2b03d8d634cdbeaf41e93cb8e9e152bff8a66
Wagubi Brian <wagubibrian@Wagubis-MacBook-Pro.local>
2018-07-05 18:46:15 +0300

    [Feature #158174601] Add site root

--- hc/settings.py
+++ hc/settings.py
@@ -145,6 +145,7 @@ USE_TZ = True
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 SITE_ROOT = os.environ.get("SITE_ROOT", "http://localhost:8000")
 =======
 SITE_ROOT = os.environ.get['SITE_ROOT']
@@ -173,6 +174,9 @@ SITE_ROOT = os.environ.get('SITE_ROOT', 'http://localhost:8000')
 =======
 SITE_ROOT = os.environ.get['SITE_ROOT']
 >>>>>>> [Feature #158174601] Enable site root to be picked from environment
+=======
+SITE_ROOT = "http://localhost:8000"
+>>>>>>> [Feature #158174601] Add site root
 PING_ENDPOINT = SITE_ROOT + "/ping/"
 PING_EMAIL_DOMAIN = HOST
 STATIC_URL = '/static/'
