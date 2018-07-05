44fb1b68163c5a9f3ebafcb750138ce36a412b26
Wagubi Brian <wagubibrian@Wagubis-MacBook-Pro.local>
2018-07-05 14:29:26 +0300

    [Feature #158174601] Add flash messages for unauthorized access

--- hc/front/views.py
+++ hc/front/views.py
@@ -18,6 +18,7 @@ from hc.api.models import DEFAULT_GRACE, DEFAULT_TIMEOUT, Channel, Check, Ping
 from hc.front.forms import (AddChannelForm, AddWebhookForm, NameTagsForm,
                     TimeoutForm, NagIntervalForm, ShopifyForm, PriorityForm)
 import shopify
+from django.contrib import messages
 
 from django.contrib import messages
 
