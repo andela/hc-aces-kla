a0b5251ee750f12e41ec85a4bbf0de4676b2967b
Wagubi Brian <wagubibrian@Wagubis-MacBook-Pro.local>
2018-07-06 11:40:42 +0300

    [Feature #158174601] Update settings spelling

--- hc/settings.py
+++ hc/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 import os
 import warnings
 import dj_database_url
-from decople import config
+from decouple import config
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
