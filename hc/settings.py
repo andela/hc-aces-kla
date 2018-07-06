f4abb4786eae7980e896c806eb9eb1d9e82f418f
Wagubi Brian <wagubibrian@Wagubis-MacBook-Pro.local>
2018-07-06 07:26:59 +0300

    [Feature #158174601] Update travis file

--- hc/settings.py
+++ hc/settings.py
@@ -130,6 +130,7 @@ USE_TZ = True
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 SITE_ROOT = os.environ.get("SITE_ROOT", "http://localhost:8000")
 =======
 SITE_ROOT = os.environ.get['SITE_ROOT']
@@ -137,6 +138,9 @@ SITE_ROOT = os.environ.get['SITE_ROOT']
 =======
 SITE_ROOT = "http://localhost:8000"
 >>>>>>> [Feature #158174601] Add site root
+=======
+SITE_ROOT = os.environ.get('SITE_ROOT')
+>>>>>>> [Feature #158174601] Update travis file
 PING_ENDPOINT = SITE_ROOT + "/ping/"
 PING_EMAIL_DOMAIN = HOST
 STATIC_URL = '/static/'
