89e4d739b42f9be99a7ec4e238c0d6d695f54adc
Wagubi Brian <wagubibrian@Wagubis-MacBook-Pro.local>
2018-07-06 07:50:32 +0300

    [Feature #158174601] Remove changes from travis

--- hc/settings.py
+++ hc/settings.py
@@ -131,6 +131,7 @@ USE_TZ = True
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 SITE_ROOT = os.environ.get("SITE_ROOT", "http://localhost:8000")
 =======
 SITE_ROOT = os.environ.get('SITE_ROOT')
@@ -141,6 +142,9 @@ SITE_ROOT = "http://localhost:8000"
 =======
 SITE_ROOT = os.environ.get('SITE_ROOT')
 >>>>>>> [Feature #158174601] Make changes to travis
+=======
+SITE_ROOT = "http://localhost:8000"
+>>>>>>> [Feature #158174601] Remove changes from travis
 PING_ENDPOINT = SITE_ROOT + "/ping/"
 PING_EMAIL_DOMAIN = HOST
 STATIC_URL = '/static/'
