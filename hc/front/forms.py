a190e9b251718257216b45ca40256a231810f6e4
matthewacha <matthewacha@gmail.com>
2018-07-11 19:23:05 +0300

    [feature] create jobs tagged to departments   - Add departments column in check model   - Edit my_check.html to reflect departments   - Edit my_checks view to return departments to context   - implement pep8 checks
    [Delivers #158174596]

--- hc/front/forms.py
+++ hc/front/forms.py
@@ -2,9 +2,10 @@ from django import forms
 from hc.api.models import Channel
 
 
-class NameTagsForm(forms.Form):
+class NameTagsDepartmentForm(forms.Form):
     name = forms.CharField(max_length=100, required=False)
     tags = forms.CharField(max_length=500, required=False)
+    department = forms.CharField(max_length=500, required=False)
 
     def clean_tags(self):
         list_a = []
