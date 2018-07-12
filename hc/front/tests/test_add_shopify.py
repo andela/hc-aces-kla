64be820fc0b4b05c84dbe62c4e7704d29fd60c18
Wagubi Brian <wagubibrian@Wagubis-MacBook-Pro.local>
2018-07-12 11:00:42 +0300

    Fix Conflicts

--- hc/front/tests/test_add_shopify.py
+++ hc/front/tests/test_add_shopify.py
@@ -6,7 +6,10 @@ from django.core.urlresolvers import reverse
 import shopify
 import requests
 from hc.api.models import Check
+<<<<<<< HEAD
 import os
+=======
+>>>>>>> Fix Conflicts
 
 class AddShopifyAlertTestCase(BaseTestCase):
     """This class contains tests to handle adding checks"""
@@ -22,9 +25,15 @@ class AddShopifyAlertTestCase(BaseTestCase):
 
     def test_it_accepts_connection_to_shopify(self):
         """test that when the correct authentication keys are given that it returns valid response """
+<<<<<<< HEAD
         API_KEY = os.environ.get('API_KEY')
 
         PASSWORD = os.environ.get('PASSWORD')
+=======
+        API_KEY = "2ea13bd64c9c06f5ff5501dd6872ecda"
+
+        PASSWORD = "d602f072d114aceca3c21a2234582ce3"
+>>>>>>> Fix Conflicts
 
         EVENT = "order/create"
 
@@ -45,7 +54,11 @@ class AddShopifyAlertTestCase(BaseTestCase):
         assert response.status_code == 302
 
     def test_it_doesnot_accept_wrong_details(self):
+<<<<<<< HEAD
         API_KEY = "84895nfjdufer0n5jnru553jdmfi9"
+=======
+        API_KEY = "2ea13bd64c9c06f5ff5501dd6872ecda"
+>>>>>>> Fix Conflicts
 
         PASSWORD = "d602f072d117438yjfjfjfu9582ce3"
 
@@ -65,9 +78,15 @@ class AddShopifyAlertTestCase(BaseTestCase):
         assert response.status_code == 403
 
     def test_it_creates_alert_for_check_shopify_and_redirects(self):
+<<<<<<< HEAD
         API_KEY = os.environ.get('API_KEY')
 
         PASSWORD = os.environ.get('PASSWORD')
+=======
+        API_KEY = "2ea13bd64c9c06f5ff5501dd6872ecda"
+
+        PASSWORD = "d602f072d114aceca3c21a2234582ce3"
+>>>>>>> Fix Conflicts
 
         EVENT = "order/create"
 
@@ -89,4 +108,28 @@ class AddShopifyAlertTestCase(BaseTestCase):
         response = self.client.get("/checks/")
         self.assertContains(response, "Create Order", status_code=200)
 
+<<<<<<< HEAD
+=======
+    def test_it_cannot_create_alert_for_similar_event(self):
+        API_KEY = "2ea13bd64c9c06f5ff5501dd6872ecda"
+
+        PASSWORD = "d602f072d114aceca3c21a2234582ce3"
+
+        EVENT = "products/create"
+
+        NAME = "Create Order"
+
+        SHOP_NAME = "Duuka1"
+
+        form = {"api_key": API_KEY,
+                "password": PASSWORD, "event": EVENT, "name": NAME, "shop_name": SHOP_NAME}
+
+        self.client.login(username="alice@example.org", password="password")
+        response = self.client.post("/checks/create_shopify_alert/", form)
+        
+        shop_url = "https://%s:%s@duuka1.myshopify.com/admin" % (
+            API_KEY, PASSWORD)
+        
+        assert response.status_code == 400
+>>>>>>> Fix Conflicts
 
--- hc/front/views.py
+++ hc/front/views.py
@@ -223,8 +223,13 @@ def create_shopify_alerts(request):
     assert request.method == "POST"
 
     form = ShopifyForm(request.POST)
-    if form.is_valid():        
-        topic = form.cleaned_data["event"]        
+    if form.is_valid():
+        check = Check(user=request.team.user)
+        check.name = form.cleaned_data["name"]
+        check.save()
+        topic = form.cleaned_data["event"]
+        
+        check_created = Check.objects.filter(name=form.cleaned_data["name"]).first()
         API_KEY = form.cleaned_data["api_key"]
         PASSWORD = form.cleaned_data["password"]
         SHOP_NAME= form.cleaned_data['shop_name']
