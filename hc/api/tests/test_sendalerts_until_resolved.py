91f856eab12926eca4bdb2986032e9e5f9e82ecd
Matthew Wacha <30747298+matthewacha@users.noreply.github.com>
2018-07-16 12:47:55 +0300

    Fix makemigrate conflicts (#30)
    * Permission for shell file
    
    * Fix migrations conflicts

--- hc/api/tests/test_sendalerts_until_resolved.py
+++ hc/api/tests/test_sendalerts_until_resolved.py
@@ -23,3 +23,6 @@ class SendAlertsUntilResolvedTestCase(BaseTestCase):
 
         result = Command().handle_many()
         self.assertEqual(result, True)
+
+    # def test_set_priority_level(self)
+    
