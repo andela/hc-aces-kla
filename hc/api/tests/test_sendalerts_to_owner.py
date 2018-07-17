ca1a5794e80fb1431fd2943d6c8d2ea48aae3d43
matthewacha <matthewacha@gmail.com>
2018-07-17 16:34:58 +0300

    [feature]allocate different team members to different checks   - add check_owner column   - send alert to check owner   - run makemigrations [Delivers #158174595]

---
+++ hc/api/tests/test_sendalerts_to_owner.py
@@ -0,0 +1,39 @@
+from datetime import timedelta
+
+from django.utils import timezone
+from django.contrib.auth.models import User
+from hc.accounts.models import Profile
+from hc.api.models import Check, Channel
+from hc.test import BaseTestCase
+from mock import patch
+from django.contrib.auth.hashers import make_password
+
+
+def fake_twilio_notify():
+    pass
+
+
+class SendOwnerAlertsTestCase(BaseTestCase):
+
+    def setUp(self):
+        super(SendOwnerAlertsTestCase,  self).setUp()
+        self.profile.token = make_password("secret-token")
+        self.profile.save()
+
+    @patch("hc.api.transports.TwilioSms.notify", fake_twilio_notify())
+    def test_it_handles_check_owner(self):
+        """
+            test it sends notifications to assigned owner
+        """
+        channel = Channel(user=self.bob, kind="email",
+                          value="bob@example.org")
+        channel.save()
+        check = Check(user=self.alice, status="down", check_owner="bob@example.org")
+        check.last_ping = timezone.now() - timedelta(minutes=30)
+        check.number_of_nags = 2
+        check.priority = 3
+        check.save()
+        check.send_alert()
+
+        assert check.escalate==False
+        assert check.number_of_nags == 3
