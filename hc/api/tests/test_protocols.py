89126d6eb3bffee1962e9f7a93cd5c2253a642b8
matthewacha <matthewacha@gmail.com>
2018-07-18 10:32:01 +0300

    Edit test_protocols

--- hc/api/tests/test_protocols.py
+++ hc/api/tests/test_protocols.py
@@ -17,48 +17,40 @@ class SendProtocolAlertsTestCase(BaseTestCase):
         super(SendProtocolAlertsTestCase,  self).setUp()
         self.profile.token = make_password("secret-token")
         self.profile.save()
+        self.channel = Channel(user=self.bob, kind="email",
+                          value="bob@example.com")
+        self.channel.save()
+        self.check = Check(user=self.alice, status="down")
+        self.check.last_ping = timezone.now() - timedelta(minutes=300)
+        self.check.number_of_nags = 5
+        self.check.priority = 3
+        self.check.save()
 
     @patch("hc.api.transports.TwilioSms.notify", fake_twilio_notify())
     def test_it_handles_protocol_list(self):
         """
             test it escalates
         """
-        channel = Channel(user=self.bob, kind="email",
-                          value="bob@example.com")
-        channel.save()
-        check = Check(user=self.alice, status="down")
-        check.last_ping = timezone.now() - timedelta(minutes=300)
-        check.number_of_nags = 5
-        check.priority = 3
-        check.save()
-        check.send_alert()
-
-        assert check.escalate
-        assert check.number_of_nags == 6
+        self.check.send_alert()
+        assert self.check.escalate
+        assert self.check.number_of_nags == 6
 
     @patch("hc.api.transports.TwilioSms.notify", fake_twilio_notify())
     def test_it_handles_many_checks_protocol_list(self):
         """
             test it escalates
         """
-        channel = Channel(user=self.bob, kind="email", value="bob@example.com")
-        channel.save()
-        check = Check(user=self.alice, status="down")
         check1 = Check(user=self.alice, status="down")
         check2 = Check(user=self.alice, status="down")
-        check.last_ping = timezone.now() - timedelta(minutes=300)
         check1.last_ping = timezone.now() - timedelta(minutes=300)
         check2.last_ping = timezone.now() - timedelta(minutes=300)
-        check.number_of_nags = 5
         check1.number_of_nags = 15
         check2.number_of_nags = 5
-        check.priority = 3
         check1.priority = 2
         check2.priority = 2
-        check.save()
         check1.save()
         check2.save()
-        check.send_alert()
+        self.check.send_alert()
         check1.send_alert()
         check2.send_alert()
 
